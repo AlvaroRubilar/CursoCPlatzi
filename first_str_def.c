6401474aad675c3818f48191ac5f917216375566
Alvaro <alvaro.rubilar@outlook.com>
2020-11-25 17:45:45 -0300

    imprime cada caracter usando punteros

--- first_str_def.c
+++ first_str_def.c
@@ -4,5 +4,10 @@ int main(int argc, char const *argv[])
 {
     char * name = "Alvaro";
     printf("nombre = %s %p \n",name,name);
+    for (int i = 0; i < 6; i++)
+    {
+       printf("name[%d](%p) = %c\n",i,name +i,*(name+i));
+    }
+    
     return 0;
 }
