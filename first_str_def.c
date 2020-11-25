d6a3a764c70ae57fdb10514e2f0ba865f264965f
Alvaro <alvaro.rubilar@outlook.com>
2020-11-25 17:47:50 -0300

    imprime la lista usando la forma clásica

--- first_str_def.c
+++ first_str_def.c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
     printf("nombre = %s %p \n",name,name);
     for (int i = 0; i < 6; i++)
     {
-       printf("name[%d](%p) = %c\n",i,name +i,*(name+i));
+       printf("name[%d](%p) = %c\n",i, name +i, name[i]);
     }
     
     return 0;
