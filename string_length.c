75c93a96faa36dc220afe2018f12ec8c16ec54a3
Alvaro <alvaro.rubilar@outlook.com>
2020-11-25 21:32:35 -0300

    imprime el largo de una cadena de caracteres

---
+++ string_length.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char * str = "Alvaro";
+    printf("La cadena mide %ld caracteres\n",strlen(str) );
+    return 0;
+}
